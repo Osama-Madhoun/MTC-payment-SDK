6e392ec677ea5736d3a2395f34195237c6a8daf7
Osama Al-Madhoun <osama98.stm@gmail.com>
2023-04-05 14:53:04 +0300

    Update Provider MTC Payment SDK

--- src/MTCPaymentsServiceProvider.php
+++ src/MTCPaymentsServiceProvider.php
@@ -3,16 +3,12 @@
 namespace MTC\Payments;
 
 use Illuminate\Support\ServiceProvider;
-use MTC\Payments\Classes\FawryPayment;
-use MTC\Payments\Classes\HyperPayPayment;
-use MTC\Payments\Classes\KashierPayment;
-use MTC\Payments\Classes\PaymobPayment;
+use MTC\Payments\Classes\StripePayment;
+use MTC\Payments\Classes\BopPayment;
 use MTC\Payments\Classes\PayPalPayment;
-use MTC\Payments\Classes\PaytabsPayment;
+use MTC\Payments\Classes\BillPayment;
 use MTC\Payments\Classes\ThawaniPayment;
-use MTC\Payments\Classes\TapPayment;
-use MTC\Payments\Classes\OpayPayment;
-use MTC\Payments\Classes\PaymobWalletPayment;
+
 
 class MTCPaymentsServiceProvider extends ServiceProvider
 {
@@ -54,11 +50,11 @@ class MTCPaymentsServiceProvider extends ServiceProvider
     public function register()
     {
 
-        $this->app->bind(PaymobPayment::class, function () {
-            return new PaymobPayment();
+        $this->app->bind(StripePayment::class, function () {
+            return new StripePayment();
         });
-        $this->app->bind(FawryPayment::class, function () {
-            return new FawryPayment();
+        $this->app->bind(BopPayment::class, function () {
+            return new BopPayment();
         });
         $this->app->bind(ThawaniPayment::class, function () {
             return new ThawaniPayment();
@@ -66,23 +62,8 @@ class MTCPaymentsServiceProvider extends ServiceProvider
         $this->app->bind(PaypalPayment::class, function () {
             return new PaypalPayment();
         });
-        $this->app->bind(HyperPayPayment::class, function () {
-            return new HyperPayPayment();
-        });
-        $this->app->bind(KashierPayment::class, function () {
-            return new KashierPayment();
-        });
-        $this->app->bind(TapPayment::class, function () {
-            return new TapPayment();
-        });
-        $this->app->bind(OpayPayment::class, function () {
-            return new OpayPayment();
-        });
-        $this->app->bind(PaymobWalletPayment::class, function () {
-            return new PaymobWalletPayment();
-        });
-        $this->app->bind(PaytabsPayment::class, function () {
-            return new PaytabsPayment();
+        $this->app->bind(BillPayment::class, function () {
+            return new BillPayment();
         });
     }
 
